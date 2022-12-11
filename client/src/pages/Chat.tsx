c7d767fc2eba2d3c041ffae70698f591955c7cc2
SAbe080601 <32716774+SAbe080601@users.noreply.github.com>
2022-12-11 13:12:52 -0500

    1

---
+++ client/src/pages/Chat.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Chat() {
+    return (
+        <div className="App">
+            Chat Component
+        </div>
+    );
+  }
+  
+  export default Chat;
