302c14ad0cd47f276415ecd698bae616f9dbbdd6
Matt Tarantino <mtarantino925@gmail.com>
2022-12-13 20:22:35 -0500

    Add channel routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,13 +1,14 @@
 import { Express, Router } from 'express'
+import channelRouter from './channels'
 
 const routers: Array<{
   route: string
   router: Router
 }> = [
-  // { route: 'user', router: userRouter },
-  // { route: 'restaurants', router: restaurantRouter },
-  // { route: 'auth', router: authRouter },
-]
+    { route: 'channels', router: channelRouter },
+    // { route: 'restaurants', router: restaurantRouter },
+    // { route: 'auth', router: authRouter },
+  ]
 
 const configRoutes = (app: Express) => {
   for (const e of routers) app.use(`/api/${e.route}`, e.router)
