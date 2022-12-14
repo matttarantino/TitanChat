02bd2399b64abb63ee6a92ab5d75025923396f31
Matt Tarantino <mtarantino925@gmail.com>
2022-12-14 18:56:26 -0500

    Add channel routes hookup

--- server/src/routes/channels.ts
+++ server/src/routes/channels.ts
@@ -1,16 +1,30 @@
 import { Router } from 'express'
-import channels from '../data/channels'
+import { getChannelById } from '../data/channels'
 import { areValidStrings } from '../utils/errors'
-// import { ensureAuthenticated } from '../middleware/auth'
+import { ensureAuthenticated } from '../middleware/auth'
 
 const channelRouter = Router()
 
 channelRouter.get('/', async (req, res) => {
-  return res.status(200).json(channels);
+  return res.status(200).json([]);
 })
 
-channelRouter.get('/:channel_id', async (req, res) => {
-  return res.status(200).json('hello tino');
+channelRouter.get('/:channelId', ensureAuthenticated, async (req, res) => {
+  const { channelId } = req.params as any
+
+  // error check
+  try {
+    areValidStrings({ channelId })
+  } catch (err) {
+    return res.status(400).send(String(err))
+  }
+
+  // send user data
+  try {
+    return res.status(200).json(await getChannelById(channelId))
+  } catch (err) {
+    return res.status(500).send(String(err))
+  }
 })
 
 export default channelRouter
