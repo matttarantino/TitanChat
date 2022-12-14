1175bea264b79831519f4b71466b444e1aa46808
SAbe080601 <32716774+SAbe080601@users.noreply.github.com>
2022-12-14 17:26:05 -0500

    Added error handling and changed a-tag to Link from react-router-dom

--- client/src/Views/components/SideBar.tsx
+++ client/src/Views/components/SideBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/sideBar.scss'
 
 import * as FaIcons from 'react-icons/fa'
@@ -11,6 +12,14 @@ import * as CgIcons from 'react-icons/cg'
 const SideBar = () => {
   const [channels, setChannels] = useState<Array<ChannelsResponse>>([])
   const [dms, setDms] = useState<Array<ChannelsResponse>>([])
+
+  // change initial state of loading to true when server is integrated
+  const [loadingChannels, setLoadingChannels] = useState(false)
+  const [loadingDms, setLoadingDms] = useState(false)
+
+  // change initial state of error to true when server is integrated
+  const [errorChannels, setErrorChannels] = useState(false)
+  const [errorDms, setErrorDms] = useState(false)
 
   let channelList = null
   let dmsList = null
@@ -45,43 +54,62 @@ const SideBar = () => {
     // get dms
 
     async function fetchChannels() {
-      const { data } = await axios.get(localChannelUrl)
-      setChannels(data)
+      try {
+        const { data } = await axios.get(localChannelUrl)
+        setChannels(data)
+        setLoadingChannels(false)
+      } catch (e) {
+        console.log('Error: ', e)
+        setLoadingChannels(false)
+        setErrorChannels(true)
+      }
     }
-    fetchChannels()
 
     async function fetchDms() {
-      const { data } = await axios.get(localDmsUrl)
-      setDms(data)
+      try {
+        const { data } = await axios.get(localDmsUrl)
+        setDms(data)
+        setLoadingDms(false)
+      } catch (e) {
+        console.log('Error: ', e)
+        setLoadingDms(false)
+        setErrorChannels(true)
+      }
     }
+
+    fetchChannels()
     fetchDms()
   }, [])
 
   channelList = ChannelSideBarData.map((elem) => {
     return (
-      <a href={`/channels/${elem.channelId}`}>
-        {' '}
+      <Link to={`/channels/${elem.channelId}`}>
         {<AiIcons.AiFillWechat />} {elem.label}
-      </a>
+      </Link>
     )
   })
 
   dmsList = DmsSideBarData.map((elem) => {
     return (
-      <a href={`/dms/${elem.channelId}`}>
-        {' '}
+      <Link to={`/dms/${elem.channelId}`}>
         {<CgIcons.CgProfile />} {elem.label}
-      </a>
+      </Link>
     )
   })
 
-  return (
-    <nav>
-      Channel SideBar
-      <div id="channelSideBar">{channelList}</div>
-      <div id="dmsSideBar">{dmsList}</div>
-    </nav>
-  )
+  if (loadingChannels && loadingDms) {
+    return <nav> Loading... </nav>
+  } else if (errorChannels && errorDms) {
+    return <nav> Error Loading Data ... </nav>
+  } else {
+    return (
+      <nav>
+        Channel SideBar
+        <div id="channelSideBar">{channelList}</div>
+        <div id="dmsSideBar">{dmsList}</div>
+      </nav>
+    )
+  }
 }
 
 export default SideBar
