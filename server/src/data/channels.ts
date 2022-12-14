0a18715a733a5cba78f719a2e56ff044f3c267a4
Matt Tarantino <mtarantino925@gmail.com>
2022-12-14 18:49:50 -0500

    add getChannelById

--- server/src/data/channels.ts
+++ server/src/data/channels.ts
@@ -1,3 +1,20 @@
+import { getChannelsCollection } from './config/mongoCollections'
 import { ObjectId } from 'mongodb'
+import { areValidStrings } from '../utils/errors'
 
-export default 'hello123'
+export const getChannelById = async (channelId: string): Promise<PublicChannel | null> => {
+  let channelIdObj
+
+  // error check
+  try {
+    areValidStrings({ channelId })
+    channelIdObj = new ObjectId(channelId)
+  } catch (err) {
+    return null
+  }
+
+  // find and return entry
+  const channelsCollection = await getChannelsCollection();
+  const channel = (await channelsCollection.findOne({ _id: channelIdObj })) as any
+  return channel ? channel : null
+}
