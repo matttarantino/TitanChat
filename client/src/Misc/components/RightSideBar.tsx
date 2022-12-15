63d68a122c5510d50317878cb29e041ce99e34f1
Edward Kashulsky <ekashulsk@stevens.edu>
2022-12-15 13:03:59 -0500

    Fixed right sidebar

--- client/src/Misc/components/RightSideBar.tsx
+++ client/src/Misc/components/RightSideBar.tsx
@@ -1,46 +1,37 @@
-import React from 'react'
-import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import '../styles/rightSideBar.scss'
-
-import * as FaIcons from 'react-icons/fa'
-import * as AiIcons from 'react-icons/ai'
-import * as BsIcons from 'react-icons/bs'
-import * as CgIcons from 'react-icons/cg'
 import { useStore } from '../../services/appStore'
+import { GetAllUsers } from '../../services/protectedService'
+import '../styles/rightSideBar.scss'
 
 const RightSideBar = () => {
   const {
     store: { authInfo },
   } = useStore()
-  const [userData, setUserData] = useState(undefined)
-
-  const usersUrl = 'http:/localhost:3001/api/users'
+  const [userData, setUserData] = useState<UserListResponse>([])
+  let usersList = null
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const { data } = await axios.get(usersUrl)
-        console.log(data)
-        console.log(data)
+        const { data } = await GetAllUsers()
         setUserData(data)
       } catch (e) {
         console.log(e)
       }
     }
     fetchData()
+  }, [])
+
+  usersList = userData.map((user) => {
+    return (
+      <div>
+        <Link to={`/dms/${user.username}`} key={user._id}>
+          {user.username}
+        </Link>
+      </div>
+    )
   })
-
-  const makeUser = (user) => {
-    return <div>{user.name}</div>
-  }
-
-  const usersList =
-    userData &&
-    userData.map((user) => {
-      return makeUser(user)
-    })
 
   if (authInfo.authenticated)
     return (
