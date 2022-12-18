79a87641cf410e972473e49dd33a2cc7e64166d4
Matt Tarantino <mtarantino925@gmail.com>
2022-12-18 10:00:09 -0500

    Chatform totally fixes

--- client/src/Channels/components/ChatForm.tsx
+++ client/src/Channels/components/ChatForm.tsx
@@ -72,7 +72,7 @@ const ChatForm = (props: Props) => {
             // reset inputs
             setMessage('')
             setImage(null)
-            ;(imageInputRef.current as HTMLInputElement).value = ''
+              ; (imageInputRef.current as HTMLInputElement).value = ''
           })
           .catch((err) => {
             console.error('aws upload error', err)
@@ -101,7 +101,9 @@ const ChatForm = (props: Props) => {
       </FloatingLabel>
 
       <div className="chat-button-bar">
+        <Form.Label htmlFor="image-upload" className="d-none">Upload a file</Form.Label>
         <Form.Control
+          id="image-upload"
           className="file-input"
           type="file"
           accept="image/*"
@@ -110,7 +112,7 @@ const ChatForm = (props: Props) => {
           ref={imageInputRef}
         />
 
-        <Button type="submit" variant="success" disabled={sendDisabled}>
+        <Button type="submit" variant={sendDisabled ? 'dark' : 'primary'} disabled={sendDisabled}>
           Send
         </Button>
       </div>
